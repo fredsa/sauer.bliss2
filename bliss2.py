de8c4d11114e6634ece134ef5378446e08c2a83d
Fred Sauer <fredsa@google.com>
2014-04-30 12:58:08 -0700

    try except for bliss2.py imports

--- bliss2.py
+++ bliss2.py
@@ -1,17 +1,22 @@
-import shlex
-import subprocess
-#import sys
-#del sys.modules['subprocess']
-#import subprocess
-import webapp2
+import logging
+logging.error('TESTING')
 
+try:
+  import shlex
+  import subprocess
+  #import sys
+  #del sys.modules['subprocess']
+  #import subprocess
+  import webapp2
+except Exception, e:
+  logging.error('Oops: {}'.format(e))
 
 
 
 class MainHandler(webapp2.RequestHandler):
 
   def post(self):
-    raise Exception(subprocess.__file__)
+    # raise Exception(subprocess.__file__)
     cmds = self.request.body.split('\n')
     for cmd in cmds:
       args = shlex.split(cmd)
